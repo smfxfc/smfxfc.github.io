c9c0d794c3a707adc61ea0c5327d4ec217391dd1
Scott Flynn-Elhoffer <57599533+smfxfc@users.noreply.github.com>
2022-05-26 16:29:48 -0700

    removed capitalized html filenames

--- src/get_cryptos.py
+++ src/get_cryptos.py
@@ -9,26 +9,31 @@ import requests
 CRYPTO_PAGE = "crypto/cryptos.md"
 BASE_URL = "https://smfxfc.github.io/crypto/"
 KEY = "https://api.binance.com/api/v3/ticker/price?symbol="
-  
+
 tokens = ["ETH", "LUNA", "NANO", "LINK", "AVAX"]
 
 html = "\n"
 for token in tokens:
-    
-    url = KEY+token+"USDT"
+
+    url = KEY + token + "USDT"
     info = requests.get(url)
     info = info.json()
-    price = float(info['price'])
-    site_url = f"[{token}]({BASE_URL}{token}.html)"
-    
+    price = float(info["price"])
+    site_url = f"[{token}]({BASE_URL}{token.lower()}.html)"
+
     if token == "LUNA":
         html += f"{site_url}: ${price:.2f} RIP :'(\n\n"
-    else:html += f"{site_url}: ${price:.2f}\n\n"
+    else:
+        html += f"{site_url}: ${price:.2f}\n\n"
 
-with open(CRYPTO_PAGE, 'r') as existing_page:
+with open(CRYPTO_PAGE, "r") as existing_page:
     previous_content = existing_page.read()
 
-new_content = re.sub(r"(?<=<!\-\-BEGINCRYPTOINPUT\-\->)[\s\S]*(?=<!\-\-ENDCRYPTOINPUT\-\->)", f"{html}", previous_content)
+new_content = re.sub(
+    r"(?<=<!\-\-BEGINCRYPTOINPUT\-\->)[\s\S]*(?=<!\-\-ENDCRYPTOINPUT\-\->)",
+    f"{html}",
+    previous_content,
+)
 
 with open(CRYPTO_PAGE, "w") as new_page:
     new_page.write(new_content)
