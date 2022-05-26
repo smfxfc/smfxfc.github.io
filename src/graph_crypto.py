bf4911cf6f67133bb9cf164f99df6401ea60fb6f
Scott Flynn-Elhoffer <57599533+smfxfc@users.noreply.github.com>
2022-05-26 15:41:38 -0700

    minor

--- src/graph_crypto.py
+++ src/graph_crypto.py
@@ -103,7 +103,7 @@ def chart(df):
         plot_bgcolor="rgba(0,0,0,0)",
         margin=dict(l=50, r=50, b=0, t=25, pad=0))
     
-    outfile = "crypto/graphs/" + SYM + ".html"
+    outfile = "crypto/" + SYM + ".html"
     fig.write_html(outfile, include_plotlyjs='cdn')
 
     
